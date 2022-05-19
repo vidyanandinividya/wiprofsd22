bbd89b33f5d5eef2b68f4a446f82bb2dfbb6f6aa
vidyanandinividya <vidyanandinividya@gmail.com>
2022-05-19 21:01:52 +0530

    Spring Boot Security
    Spring Boot Security using JWT

---
+++ WebFrameworks/WiproSpringSecurityDemo/src/main/java/wiprofsd/webdev/WiproSpringSecurityDemoApplication.java
@@ -0,0 +1,15 @@
+package wiprofsd.webdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WiproSpringSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiproSpringSecurityDemoApplication.class, args);
+	}
+	
+
+}
